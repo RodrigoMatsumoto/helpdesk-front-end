d59d0a4e62bd2f5e5d859121e525c73d5261e2e2
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-02-01 10:30:25 -0300

    criar componente HEADER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,12 +22,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

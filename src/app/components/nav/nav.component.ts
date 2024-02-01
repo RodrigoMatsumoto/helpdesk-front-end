608955533c46738a144a33e80360d64f6160232e
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.comgit>
2024-02-01 12:07:29 -0300

    HTML e CSS para componente TECNICO-LIST

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -12,6 +12,6 @@ export class NavComponent implements OnInit {
   constructor(private router: Router) { }
   
   ngOnInit(): void {
-    this.router.navigate(['home']);
+    this.router.navigate(['tecnicos']);
   }
 }
